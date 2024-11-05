8a9e7a2da241a98510af72af3d31e37bdce9ae48
danwald <danwald79@gmail.com>
2024-11-05 16:25:21 +0400

    add min_secs env override to skip quick prs

--- git-stats/merge-stats.sh
+++ git-stats/merge-stats.sh
@@ -2,21 +2,24 @@
 
 # Navigate to the repository directory
 pushd $1 > /dev/null || { echo "Repository not found"; exit 1; }
-BRANCH=${2:-staging}
-$FROM_DATE=${FROM_DATE:-2024-07-01}
-$START_DT=${TO_DATE:-2024-10-01}
-$END_DT=${TO_DATE:-2024-10-01}
+BRANCH=${2:-main}
+STATE_DATE=${START_DATE:=2024-07-01}
+END_DATE=${END_DATE:=2024-10-01}
+MIN_SECS=${MIN_SECS:=432000} # 5 days
 
-echo "Fetching repository $1 @$BRANCH [$START_DT-$END_DT]..."
+echo "Fetching repository $1 @$BRANCH[$START_DATE:$END_DATE]..."
 git fetch origin $BRANCH staging > /dev/null 2>&1 || { echo "Failed to fetch repository $1"; exit 1; }
 
-merges=`git log --merges --pretty=format:"%h;%ct" --author='^(?!github-actions).*$' --perl-regexp --since="$START_DT" --before="$END_DT" origin/$BRANCH`
+merges=`git log --merges --pretty=format:"%h;%ct" --author='^(?!github-actions).*$' --perl-regexp --since="$START_DATE" --before="$END_DATE" origin/$BRANCH`
 durations=()
 for merge in $merges; do
     sha=`echo $merge | cut -d';' -f1`
     closed=`echo $merge | cut -d';' -f2`
     opened=`git log "$sha^1..$sha^2" --format="%ct" | tail -1`
     duration=$((closed-opened))
+    if [ $duration -lt $MIN_SECS ]; then
+        continue
+    fi
     durations+=($duration)
 done
 #echo ${durations[@]}
